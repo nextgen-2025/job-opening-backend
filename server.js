0b9b40b8dd754b32a7b171bbee0bcfdd9aecdf51
nextgen-2025 <digitalnextgeninfratech@gmail.com>
2025-04-11 15:52:53 +0530

    updated port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
